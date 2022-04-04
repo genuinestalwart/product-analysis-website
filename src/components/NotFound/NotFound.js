4a2b5eccf2783b64f40b71c15bac95163e5b7cbd
genuinestalwart <genuinestalwart@gmail.com>
2022-04-04 19:37:30 +0600

    404 page added

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <div>
-            not found
+        <div className='py-16 text-center'>
+            <h1 className='font-medium my-16 text-5xl text-slate-600'><span className='text-red-400'>Error 404</span> - Page Not Found!</h1>
+            <h2 className='font-medium my-12 text-4xl text-slate-500'>Back to <Link className='decoration-indigo-400 decoration-2 text-indigo-500 underline underline-offset-8' to='/'>main page</Link></h2>
         </div>
     );
 };
