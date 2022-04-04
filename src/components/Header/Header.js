ef67588baf340cd9ac09306d7df5d0cc0e25dd4b
genuinestalwart <genuinestalwart@gmail.com>
2022-04-04 11:16:30 +0600

    routes setup done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <header className='h-20 flex items-center justify-center'>
             <nav>
                 <ul className='decoration-2 decoration-indigo-400 flex text-lg text-slate-500 underline underline-offset-4'>
-                    <li className='mx-4'>Home</li>
-                    <li className='mx-4'>Reviews</li>
-                    <li className='mx-4'>Dashboard</li>
-                    <li className='mx-4'>Blogs</li>
+                    <li className='mx-4'><Link to='/'>Home</Link></li>
+                    <li className='mx-4'><Link to='/reviews'>Reviews</Link></li>
+                    <li className='mx-4'><Link to='/dashboard'>Dashboard</Link></li>
+                    <li className='mx-4'><Link to='/blogs'>Blogs</Link></li>
                 </ul>
             </nav>
         </header>
